94255e4007456cb1f75459f842cd15a0b1a17212
Rolf Turner <r.turner@auckland.ac.nz>
2023-11-20 12:57:51 +1300

    No clue what I'm doing.

--- src/acchk.f
+++ src/acchk.f
@@ -1,4 +1,5 @@
 C Output from Public domain Ratfor, version 1.03
+C
       subroutine acchk(i,j,k,anticl,x,y,ntot,eps)
       implicit double precision(a-h,o-z)
       dimension x(-3:ntot), y(-3:ntot), xt(3), yt(3)
